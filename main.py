c2506ccb1c646cd5283585e4a1b110608582ebe6
Miko <75154223+Achu2234@users.noreply.github.com>
2021-12-08 22:07:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from asyncio import run
 
 from loguru import logger
 from pyrogram import Client, idle
-
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 async def main():
     bot_client = Client("bot")
@@ -15,6 +15,33 @@ async def main():
     logger.debug(f"Stopping bot instance \"{bot_info.username}\" ...")
     await bot_client.stop()
     logger.info(f"Bot instance \"{bot_info.username}\" stopped.")
+    
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Client.on_message(filters.command(["start", "start@AntiChannel_RBot"]))
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
+ `Heya I'm A Anti Channel Telegram bot Just add me to the chat, and I will block the channels that write to the chat.`""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "📢 Cʜᴀɴɴᴇʟ", url="https://t.me/DeeCodeBots"
+                    ),
+                    InlineKeyboardButton(
+                        "Sᴜᴘᴘᴏʀᴛ 👥", url="https://t.me/DeCodeSupport"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🧑‍💻 Dᴇᴠ 🧑‍💻", url="https://t.me/DeeCodeDevs"
+                    )
+                ]
+            ]
+        )
+    )
 
 
 if __name__ == '__main__':
