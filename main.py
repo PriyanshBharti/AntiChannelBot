ca56dc4f6a5a46cee4e044d05aa61735cb254a06
Miko <75154223+Achu2234@users.noreply.github.com>
2021-12-08 22:29:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,33 +16,5 @@ async def main():
     await bot_client.stop()
     logger.info(f"Bot instance \"{bot_info.username}\" stopped.")
     
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-
-@Client.on_message(filters.command(["start", "start@AntiChannel_RBot"]))
-async def start(_, message: Message):
-    await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
- `Heya I'm A Anti Channel Telegram bot Just add me to the chat, and I will block the channels that write to the chat.`""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "📢 Cʜᴀɴɴᴇʟ", url="https://t.me/DeeCodeBots"
-                    ),
-                    InlineKeyboardButton(
-                        "Sᴜᴘᴘᴏʀᴛ 👥", url="https://t.me/DeCodeSupport"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🧑‍💻 Dᴇᴠ 🧑‍💻", url="https://t.me/DeeCodeDevs"
-                    )
-                ]
-            ]
-        )
-    )
-
-
 if __name__ == '__main__':
     run(main())
